1e29cc10ebf112144de5de3498df0dc9d2d81db8
SHIELDJY <lijiayi19971127@hotamail.com>
2019-07-02 18:16:55 +0800

    update

--- USER/stm32f10x_it.c
+++ USER/stm32f10x_it.c
@@ -1,14 +1,14 @@
 /**
   ******************************************************************************
-  * @file    Project/Template/stm32f10x_it.c 
+  * @file    Project/STM32F10x_StdPeriph_Template/stm32f10x_it.c 
   * @author  MCD Application Team
-  * @version V3.0.0
-  * @date    04/06/2009
+  * @version V3.5.0
+  * @date    08-April-2011
   * @brief   Main Interrupt Service Routines.
   *          This file provides template for all exceptions handler and 
   *          peripherals interrupt service routine.
   ******************************************************************************
-  * @copy
+  * @attention
   *
   * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
   * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
@@ -17,13 +17,15 @@
   * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
   * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
   *
-  * <h2><center>&copy; COPYRIGHT 2009 STMicroelectronics</center></h2>
-  */ 
+  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
+  ******************************************************************************
+  */
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f10x_it.h"
+#include "Traffic_Light.h"
 
-/** @addtogroup Template_Project
+/** @addtogroup STM32F10x_StdPeriph_Template
   * @{
   */
 
@@ -39,100 +41,76 @@
 /******************************************************************************/
 
 /**
-  * @brief  This function handles NMI exception.
-  * @param  None
-  * @retval : None
-  */
-void NMI_Handler(void)
-{
-}
-
-/**
-  * @brief  This function handles Hard Fault exception.
-  * @param  None
-  * @retval : None
-  */
-void HardFault_Handler(void)
-{
-  /* Go to infinite loop when Hard Fault exception occurs */
-  while (1)
-  {
-  }
-}
-
-/**
-  * @brief  This function handles Memory Manage exception.
-  * @param  None
-  * @retval : None
-  */
-void MemManage_Handler(void)
-{
-  /* Go to infinite loop when Memory Manage exception occurs */
-  while (1)
-  {
-  }
-}
-
-/**
-  * @brief  This function handles Bus Fault exception.
-  * @param  None
-  * @retval : None
-  */
-void BusFault_Handler(void)
-{
-  /* Go to infinite loop when Bus Fault exception occurs */
-  while (1)
-  {
-  }
-}
-
-/**
-  * @brief  This function handles Usage Fault exception.
-  * @param  None
-  * @retval : None
-  */
-void UsageFault_Handler(void)
-{
-  /* Go to infinite loop when Usage Fault exception occurs */
-  while (1)
-  {
-  }
-}
-
-/**
-  * @brief  This function handles SVCall exception.
-  * @param  None
-  * @retval : None
-  */
-void SVC_Handler(void)
-{
-}
-
-/**
-  * @brief  This function handles Debug Monitor exception.
-  * @param  None
-  * @retval : None
-  */
-void DebugMon_Handler(void)
-{
-}
-
-/**
-  * @brief  This function handles PendSVC exception.
-  * @param  None
-  * @retval : None
-  */
-void PendSV_Handler(void)
-{
-}
-
-/**
   * @brief  This function handles SysTick Handler.
   * @param  None
-  * @retval : None
+  * @retval None
   */
 void SysTick_Handler(void)
 {
+}
+
+/**
+  * @brief  This function handles TIMER2 Handler.
+  * @param  None
+  * @retval None
+  */
+uint32_t j=1;
+uint32_t t=0;
+uint32_t h=0;
+
+void TIM2_IRQHandler(void)
+{
+if (TIM_GetITStatus(TIM2, TIM_FLAG_Update)!=RESET)
+{ 
+  	if (t == 0)
+	{
+		if (j == 0)//check
+			TestRun();			
+		else if (j > 0 )
+		{
+			//Segment_led(j);
+			t = circle(j,t);		
+		}//first circle is end.
+	}
+	else
+	{
+		//Segment_led(j);	
+		t = circle(j,t);
+	}
+	
+	j++;
+	if (j>80000)
+		j=0;
+ 	
+	TIM_ClearFlag(TIM2, TIM_FLAG_Update);
+	}
+}
+
+/**
+  * @brief  This function handles USART1 Handler.
+  * @param  None
+  * @retval None
+  */
+ #define RST_ALL_LIGHTS 0xFF
+void USART1_IRQHandler( void )
+{   
+	uint16_t Res_Val = 0;
+
+    if(USART_GetITStatus(USART2, USART_IT_RXNE) != RESET)
+    {
+		Res_Val = ESP8266_ReceiveValue();
+		switch (Res_Val)
+		{
+		case RST_ALL_LIGHTS:
+			TestRun();
+			break;
+		
+		default:
+			break;
+		}
+		USART_ClearITPendingBit(USART1, USART_IT_RXNE);
+	}	
+	
 }
 
 /******************************************************************************/
@@ -145,7 +123,7 @@ void SysTick_Handler(void)
 /**
   * @brief  This function handles PPP interrupt request.
   * @param  None
-  * @retval : None
+  * @retval None
   */
 /*void PPP_IRQHandler(void)
 {
@@ -156,4 +134,4 @@ void SysTick_Handler(void)
   */ 
 
 
-/******************* (C) COPYRIGHT 2009 STMicroelectronics *****END OF FILE****/
+/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
